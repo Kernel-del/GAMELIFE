08beb9c658c7f833d0829375e9f4db7cf7ba4ef9
Kernel-del <khandrykinartur@gmail.com>
2025-03-09 16:22:08 +0300

    Added multithreading

--- Makefile
+++ Makefile
@@ -1,12 +1,12 @@
 all:
-	g++ main.cpp -std=c++11 -static -pthread -s -DLINUX -o bin/GAMELIFE
+	g++ main.cpp -std=c++11 -static -pthread -s -O3 -DLINUX -o bin/GAMELIFE
 	chmod +x bin/GAMELIFE
 
 win64:
-	x86_64-w64-mingw32-g++ main.cpp -std=c++11 -static -pthread -s -o bin/GAMELIFE-64.exe
+	x86_64-w64-mingw32-g++ main.cpp -std=c++11 -static -pthread -s -O3 -o bin/GAMELIFE-64.exe
 
 win32:
-	i686-w64-mingw32-g++ main.cpp -std=c++11 -static -pthread -s -o bin/GAMELIFE-32.exe
+	i686-w64-mingw32-g++ main.cpp -std=c++11 -static -pthread -s -o -O3 bin/GAMELIFE-32.exe
 
 clean:
 	rm -f GAMELIFE
--- src/game.hpp
+++ src/game.hpp
@@ -1,6 +1,29 @@
 #include "import_lib.hpp"
 #include "field.hpp"
 
+void step_line(Field& base, Field& tmp, int iy, int x, int y) {
+    for (int ix = 0; ix < x; ++ix) {
+        int health = 0;
+
+        for (int jy = 0; jy < 3; ++jy) {  // Подсчёт соседий клетки
+            for (int jx = 0; jx < 3; ++jx) {
+                health+=base[iy-1+jy][ix-1+jx];
+            }
+        }
+
+        if (base[iy][ix]) { // Если клетка жива, обрабатываем её СмЕрТь 
+            if (health<3 or health>4) {
+                tmp[iy][ix] = 0;
+            } else {tmp[iy][ix] = 1;}
+        } else { // Иначе рождаем или воскрешаем её :)
+            if (health==3) {
+                tmp[iy][ix] = 1;
+            } else {
+                tmp[iy][ix] = 0;
+            }
+        }
+    }
+}
 
 // Основной движок игры
 class Game{
@@ -9,10 +32,14 @@ class Game{
         int x = 0;
         Field* base= new Field[2];
         bool selector = 0; // Выбор основной карты и tmp карты
+
+        std::vector<std::thread> threads;
     public:
         Game(int x = 10, int y = 10): x(x),  y(y){
             base[0] = Field(x, y, 0);
             base[1] = Field(x, y, 0);
+
+            std::vector<std::thread> threads(y);
         }
 
         // Вывод содержимого основной карты
@@ -31,30 +58,20 @@ class Game{
         // Обработка хода
         Game& step() {
             for (int iy = 0; iy < y; ++iy) {
-                for (int ix = 0; ix < x; ++ix) {
-                    int health = 0;
-
-                    for (int jy = 0; jy < 3; ++jy) {  // Подсчёт соседий клетки
-                        for (int jx = 0; jx < 3; ++jx) {
-                            health+=base[selector][iy-1+jy][ix-1+jx];
-                        }
-                    }
-
-                    if (base[selector][iy][ix]) { // Если клетка жива, обрабатываем её СмЕрТь 
-                        if (health<3 or health>4) {
-                            base[!selector][iy][ix] = 0;
-                        } else {base[!selector][iy][ix] = 1;}
-                    } else { // Иначе рождаем или воскрешаем её :)
-                        if (health==3) {
-                            base[!selector][iy][ix] = 1;
-                        } else {
-                            base[!selector][iy][ix] = 0;
-                        }
-                    }
-                }
+                threads.emplace_back(step_line, std::ref(base[selector]), std::ref(base[!selector]), iy, x, y);
             }
+
+            for (int iy = 0; iy < y; ++iy) {
+                threads[iy].join();
+            }
+            threads.clear();
 
             selector=!selector; // Переключение выбора основной карты
             return *this;
+        }
+        
+
+        ~Game() {
+            threads.clear();
         }
 };
